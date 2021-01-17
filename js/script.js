87b3a14b6d4cfe5f6f9cbfbc4c11a84d3f8a0787
Geo-hot <55334979+Shrapnel-max@users.noreply.github.com>
2021-01-17 17:29:14 +0200

    Modified the structure of the application
    Without the structured application. Github pages cannot display the application in the manner of our preference.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function togglePassword() {
+  let password = document.getElementById("password");
+  let showPassword = document.getElementById("show");
+  let hidePassword = document.getElementById("hide");
+
+  if (password.type === "password") {
+    password.type = "text";
+    showPassword.style.display = "block";
+    hidePassword.style.display = "none";
+  } else {
+    password.type = "password";
+    showPassword.style.display = "none";
+    hidePassword.style.display = "block";
+  }
+}
